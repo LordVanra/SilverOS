186f7e4e9cb523a86215a9598a6b9118fc6ad770
Donya Darawcheh <dedarawcheh@wpi.edu>
2026-05-08 20:24:45 -0400

    Update Files.java

--- Files.java
+++ Files.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 
+/*
+ * Class that creates a JFrame window for the Files tutorial.
+ */
 public class Files {
 	
 	private int preferredWidth = 800;
@@ -14,10 +17,16 @@ public class Files {
 			+ "<b>How to delete a file</b><br>"
 			+ "Click on the file which you desire to erase and click the delete button on your keyboard.</p>";
 
+	/*
+	 * Opens the Files tutorial window/runs the code.
+	 */
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> new Files().createUI());
 	}
 
+	/*
+	 * Creates the JFrame, scrollPane, and conentPanel sizings and is also in charge of the main text and image layout.
+	 */
 	private void createUI() {
 		JFrame frame = new JFrame("Tutorials for Files");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,6 +61,11 @@ public class Files {
 	}
 
 	// Create wrapped, centered text
+	/*
+	 * Takes the String text and converts it into a JLabel with html before aligning (center align) and sizing the text.
+	 * @param text: A string that contains the text of the tutorial
+	 * @return returns the resulting Jlabel with the wrapped, center-aligned text
+	 */
 	private JComponent createText(String text) {
 		JLabel label = new JLabel(
 				"<html><div style='text-align:center; width:600px; font-size:20px;'>" + text + "</div></html>");
